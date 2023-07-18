2b1c149b694ada005981b5609d12620b72f980d5
GarytheNoob on Archlinux <garythenoob@outlook.com>
2023-07-18 17:45:29 +0800

    plug surround

--- lua/config/plugin.lua
+++ lua/config/plugin.lua
@@ -137,6 +137,17 @@ require("lazy").setup({
     },
 
     {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
+
+    {
         "lewis6991/gitsigns.nvim",
         config = function()
             require('gitsigns').setup {
