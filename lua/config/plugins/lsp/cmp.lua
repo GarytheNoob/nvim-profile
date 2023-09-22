916eade4e8d8cd2f390317977d4ca82962829d79
GarytheNoob on ArchLinux <garythenoob@outlook.com>
2023-09-23 00:29:32 +0800

    add border to cmp tooltip

--- lua/config/plugins/lsp/cmp.lua
+++ lua/config/plugins/lsp/cmp.lua
@@ -32,6 +32,14 @@ return {
                         require('luasnip').lsp_expand(args.body)
                     end,
                 },
+                window = {
+                    completion = {
+                        border = "rounded",
+                    },
+                    documentation = {
+                        border = "rounded",
+                    },
+                },
                 mapping = cmp.mapping.preset.insert({
                     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
