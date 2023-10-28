bb049259088c05a4d9c3cef42d45626bfa0f8480
GarytheNoob on ArchLinux <garythenoob@outlook.com>
2023-10-28 14:19:56 +0800

    config vimtex

--- lua/config/plugins/latex.lua
+++ lua/config/plugins/latex.lua
@@ -2,8 +2,12 @@ return {
     {
         "lervag/vimtex",
         config = function()
-            vim.cmd("let g:vimtex_view_general_viewer = 'zathura'")
-            vim.cmd("let g:vimtex_view_method = 'zathura'")
+            vim.cmd([[
+                let g:tex_flavor = 'latex'
+                let g:vimtex_quickfix_mode = 0
+                let g:vimtex_view_general_viewer = 'zathura'
+                let g:vimtex_view_method = 'zathura'
+            ]])
         end
     },
 }
