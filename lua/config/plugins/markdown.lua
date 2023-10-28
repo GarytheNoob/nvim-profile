17598b2d7a216f94547db2cc2e284ca4e0f5ffbd
GarytheNoob on ArchLinux <garythenoob@outlook.com>
2023-10-28 15:08:49 +0800

    plug FrMaco

--- lua/config/plugins/markdown.lua
+++ lua/config/plugins/markdown.lua
@@ -1,7 +1,15 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    build = function()
-        vim.fn["mkdp#util#install"]()
-    end,
-    ft = "markdown",
+    {
+        "iamcco/markdown-preview.nvim",
+        build = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+        ft = "markdown",
+    },
+    {
+        'AckslD/nvim-FeMaco.lua',
+        config = function()
+            require("femaco").setup()
+        end
+    }
 }
