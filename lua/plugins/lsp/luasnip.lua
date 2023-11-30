0f753cf5556eb183c0f5d2ae0fb5f7792a0547b5
GarytheNoob on ArchLinux <garythenoob@outlook.com>
2023-11-30 16:18:14 +0800

    change: add custom snippets in LuaSnip

---
+++ lua/plugins/lsp/luasnip.lua
@@ -0,0 +1,33 @@
+return {
+    "L3MON4D3/LuaSnip", -- snippet engine
+
+    config = function ()
+        local status_ok, ls= pcall(require, "luasnip")
+        if not status_ok then
+            return
+        end
+
+        require("luasnip.loaders.from_lua").load(
+            { paths = vim.fn.expand(vim.fn.stdpath('config') .. '/lua/custom/snippets')}
+        )
+
+        local types = require("luasnip.util.types")
+        ls.config.set_config({
+            history = true, --keep around last snippet local to jump back
+            updateevents = "TextChanged,TextChangedI", --update changes as you type
+            enable_autosnippets = true,
+            ext_opts = {
+                [types.choiceNode] = {
+                    active = {
+                        virt_text = { { "●", "#D19A66" } },
+                    },
+                },
+                -- [types.insertNode] = {
+                -- 	active = {
+                -- 		virt_text = { { "●", "GruvboxBlue" } },
+                -- 	},
+                -- },
+            },
+        })
+    end
+}
--- lua/plugins/lsp/nvim-cmp.lua
+++ lua/plugins/lsp/nvim-cmp.lua
@@ -4,7 +4,7 @@ return {
     dependencies = {
         "hrsh7th/cmp-buffer", -- source for text in buffer
         "hrsh7th/cmp-path", -- source for file system paths
-        "L3MON4D3/LuaSnip", -- snippet engine
+        -- "L3MON4D3/LuaSnip", -- snippet engine
         "saadparwaiz1/cmp_luasnip", -- for autocompletion
         "rafamadriz/friendly-snippets", -- useful snippets
         "onsails/lspkind.nvim", -- vs-code like pictograms
@@ -23,7 +23,6 @@ return {
             completion = {
                 completeopt = "menu,menuone,preview,noselect",
             },
-
             window = { -- add border to the tooltip window of cmp
                 completion = {
                     border = "rounded",
