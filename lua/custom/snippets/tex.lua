a6e2099f37d35498b1a702abc8ff357a56a68787
GarytheNoob on ArchLinux <garythenoob@outlook.com>
2023-12-01 12:16:41 +0800

    change: snippet for TeX - change setting style

--- lua/custom/snippets/tex.lua
+++ lua/custom/snippets/tex.lua
@@ -11,133 +11,75 @@ local sn = ls.snippet_node
 local fmt = require("luasnip.extras.fmt").fmt
 local rep = require("luasnip.extras").rep
 
-local snippets, autosnippets = {}, {}
-
-local group = vim.api.nvim_create_augroup("TeX Snippets", { clear = true })
-local file_pattern = "*.tex"
-
-local function cs(trigger, nodes, opts)
-	local snippet = s(trigger, nodes)
-	local target_table = snippets
-
-	local pattern = file_pattern
-	local keymaps = {}
-
-	if opts ~= nil then
-		-- check for custom pattern
-		if opts.pattern then
-			pattern = opts.pattern
-		end
-
-		-- if opts is a string
-		if type(opts) == "string" then
-			if opts == "auto" then
-				target_table = autosnippets
-			else
-				table.insert(keymaps, { "i", opts })
-			end
-		end
-
-		-- if opts is a table
-		if opts ~= nil and type(opts) == "table" then
-			for _, keymap in ipairs(opts) do
-				if type(keymap) == "string" then
-					table.insert(keymaps, { "i", keymap })
-				else
-					table.insert(keymaps, keymap)
-				end
-			end
-		end
-
-		-- set autocmd for each keymap
-		if opts ~= "auto" then
-			for _, keymap in ipairs(keymaps) do
-				vim.api.nvim_create_autocmd("BufEnter", {
-					pattern = pattern,
-					group = group,
-					callback = function()
-						vim.keymap.set(keymap[1], keymap[2], function()
-							ls.snip_expand(snippet)
-						end, { noremap = true, silent = true, buffer = true })
-					end,
-				})
-			end
-		end
-	end
-
-	table.insert(target_table, snippet) -- insert snippet into appropriate table
+local function is_math()
+    return vim.fn["vimtex#syntax#in_mathzone"]()
 end
 
-
-cs(
-    "documentclass",
-    {
-        t('\\documentclass{'),
-        i(1, ""),
-        t('}'),
-    },
-    "docc"
-)
-
-cs(
-    "usepackage",
-    fmt(
-        [[
+local snippets = {
+    s(
+        "documentclass",
+        {
+            t('\\documentclass{'),
+            i(1, ""),
+            t('}'),
+        }
+    ),
+    s(
+        "usepackage",
+        fmt(
+            [[
             \usepackage{}{{{}}}
         ]],
-        {
-            c(1, {
-                t(""),
-                sn(1,{
-                    t("["),
-                    i(1, ""),
-                    t("]")
-                })
-            }),
-            i(2, "")
-        }
-    ),
-    "pkg"
-)
+            {
+                c(1, {
+                    t(""),
+                    sn(1,{
+                        t("["),
+                        i(1, ""),
+                        t("]")
+                    })
+                }),
+                i(2, "")
+            }
+        )
+    )
+}
 
-cs(
-    "frac",
-    fmt(
-        [[
+local autosnippets = {
+    s(
+        "//",
+        fmt(
+            [[
             \frac{{{}}}{{{}}}
         ]],
+            {
+                i(1, ""),
+                i(2, ""),
+            }
+        ),
         {
-            i(1, ""),
-            i(2, ""),
+            condition = is_math -- TODO: WHY THIS DOESN'T WORK??
         }
     ),
-    "//"
-)
-
-cs(
-    {
-        trig = "differential",
-        hidden = true,
-    },
-    {
-        t('{\\mathkrm d}')
-    },
-    "dd"
-)
-
-cs(
-    {
-        trig = "spaced_differential",
-        hidden = true,
-    },
-    {
-        t('\\,{\\mathkrm d}')
-    },
-    ",dd"
-)
-
-
-
+    s(
+        "dd",
+        {
+            t('{\\mathrm d}')
+        },
+        {
+            condition = is_math
+        }
+    ),
+    s(
+        "spd",
+        {
+            t('\\,{\\mathrm d}')
+        },
+        {
+            condition = is_math
+        }
+    )
+}
 
 
 return snippets, autosnippets
