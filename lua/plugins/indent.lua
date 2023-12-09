b8ab344ecf570ceb4eedfdf421a81465c03f93de
GarytheNoob on ArchLinux <garythenoob@outlook.com>
2023-12-09 20:40:52 +0800

    change: replace ibl with hlchunk

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,12 +1,39 @@
-return {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    config = function()
-	    require("ibl").setup({
-            scope = {
-                show_start = false,
-                show_end = false,
-            },
-        })
-    end
+return { 
+    {
+		"shellRaining/hlchunk.nvim",
+		init = function()
+			vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, { pattern = "*", command = "EnableHL", })
+			require('hlchunk').setup({
+				chunk = {
+					enable = true,
+					use_treesitter = true,
+					style = {
+						{ fg = "#7f848e" },
+					},
+				},
+				indent = {
+					chars = { "│", "¦", "┆", "┊", },
+					use_treesitter = false,
+				},
+				blank = {
+					enable = false,
+				},
+				line_num = {
+					use_treesitter = true,
+				},
+			})
+		end
+    },
+    -- {
+    --     "lukas-reineke/indent-blankline.nvim",
+    --     main = "ibl",
+    --     config = function()
+    --         require("ibl").setup({
+    --             scope = {
+    --                 show_start = false,
+    --                 show_end = false,
+    --             },
+    --         })
+    --     end
+    -- }
 }
