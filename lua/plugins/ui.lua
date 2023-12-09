f34725bd641a1b64738a0e9e11cc669e338eb095
GarytheNoob on ArchLinux <garythenoob@outlook.com>
2023-12-09 16:14:23 +0800

    add: plug colorful-winsep and conf

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,6 +1,26 @@
 -- We all love making the ui better
 
 return {
+	{
+		"nvim-zh/colorful-winsep.nvim",
+		config = function ()
+            local colorful_winsep = require("colorful-winsep")
+            colorful_winsep.setup({
+                no_exec_files = { "packer", "TelescopePrompt", "mason", "CompetiTest", "NvimTree", "Outline", "no-neck-pain"},
+                create_event = function() -- disable in NvimTree
+                    local win_n = require("colorful-winsep.utils").calculate_number_windows()
+                    if win_n == 2 then
+                        local win_id = vim.fn.win_getid(vim.fn.winnr('h'))
+                        local filetype = vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(win_id), 'filetype')
+                        if filetype == "NvimTree" then
+                            colorful_winsep.NvimSeparatorDel()
+                        end
+                    end
+                end
+            })
+		end,
+		event = { "WinNew" },
+	},
     {
         "stevearc/dressing.nvim",
         event = "VeryLazy"
