eb66f4da57fb0a7d96e8c68a7c632875f28844cf
GarytheNoob on ArchLinux <garythenoob@outlook.com>
2023-12-09 20:41:12 +0800

    change: use global statusline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,6 +5,8 @@ return { -- lualine
         require("lualine").setup({
             options = {
                 theme = 'auto',
+				always_divide_middle = true,
+				globalstatus = true,
                 ignore_focus = { "NvimTree", "Outline", "no-neck-pain" },
             },
         })
