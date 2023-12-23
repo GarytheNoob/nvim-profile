542ddf7cce3a027707c088aee92e7de594730626
GarytheNoob on ArchLinux <garythenoob@outlook.com>
2023-12-23 15:56:08 +0800

    add: plug color-picker

---
+++ lua/plugins/color.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "ziontee113/color-picker.nvim",
+        config = function()
+            require("color-picker").setup({
+                ["icons"] = { "◼️", "▶" },
+            })
+        end,
+    }
+}
