a378488c0701e75b5368726526c466a5e80075bb
GarytheNoob on ArchLinux <garythenoob@outlook.com>
2024-06-23 13:04:27 +0800

    plug jieba.nvim

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -23,4 +23,13 @@ return {
 			})
 		end
 	},
+    {
+        'noearc/jieba.nvim',
+        dependencies = {'noearc/jieba-lua'},
+        config = function ()
+            vim.keymap.set('n', 'ce', ":lua require'jieba_nvim'.change_w()<CR>", {noremap = false, silent = true})
+            vim.keymap.set('n', 'de', ":lua require'jieba_nvim'.delete_w()<CR>",  {noremap = false, silent = true})
+            vim.keymap.set('n', '<leader>w' , ":lua require'jieba_nvim'.select_w()<CR>", {noremap = false, silent = true})
+        end
+    },
 }
