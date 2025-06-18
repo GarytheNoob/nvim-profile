8c7c508033783ad2f48482ea3b7148e4cd23a60b
GarytheNoob on ArchLinux <garythenoob@outlook.com>
2025-06-18 15:25:58 +0800

    change: ignore downloading ipkg treesitter - unable to download

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
     {"lincheney/nvim-ts-rainbow"}, -- NOTE: Change origin - p00f stopped maintaining
     { -- treesitter
         "nvim-treesitter/nvim-treesitter",
+        -- branch = 
         config = function()
             require 'nvim-treesitter.install'.compilers = { 'clang' }
 
@@ -10,7 +11,7 @@ return {
                 ensure_installed = "all",
                 auto_install = true,
                 sync_install = true,
-                ignore_install = {},
+                ignore_install = {"ipkg",},
                 modules = {},
                 highlight = {
                     enable = true,
