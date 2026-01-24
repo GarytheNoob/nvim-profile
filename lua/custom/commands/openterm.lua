877f3766ba5efcce653a5a1e9bae63167e63c205
ZhangXuecheng <zxc0gary@connect.hku.hk>
2026-01-24 14:49:11 +0800

    change: OOP style openterm

--- lua/custom/commands/openterm.lua
+++ lua/custom/commands/openterm.lua
@@ -1,9 +1,20 @@
 return {
     cmd = 'OpenTerm',
     func = function()
+        local Terminal = require('toggleterm.terminal').Terminal
+
         local current_file_directory = vim.fn.expand("%:p:h")
-        local term_command = string.format("ToggleTerm direction=horizontal size=20 cwd=%s",
-            vim.fn.shellescape(current_file_directory))
-        vim.cmd(term_command)
+        local term = Terminal:new({
+            direction = "horizontal",
+            dir = current_file_directory,
+            env = {
+                termapp = "nvim",
+            },
+            close_on_exit = true,
+        })
+        -- local term_command = string.format("ToggleTerm direction=horizontal size=20 cwd=%s",
+        --     vim.fn.shellescape(current_file_directory))
+        -- vim.cmd(term_command)
+        term:toggle()
     end
 }
