d2798e780aba12c0968cc950d1805850273f2842
ZhangXuecheng <zxc0gary@connect.hku.hk>
2026-03-23 16:58:07 +0800

    add: c++ compile flags

--- lua/custom/commands/runcode.lua
+++ lua/custom/commands/runcode.lua
@@ -9,33 +9,33 @@ local split = function()
     ]])
 end
 local compileRun = function()
-	vim.cmd("w")
+    vim.cmd("w")
     vim.cmd("lcd " .. vim.fn.expand("%:p:h"))
-	-- check file type
-	local ft = vim.bo.filetype
-	if ft == "python" then
+    -- check file type
+    local ft = vim.bo.filetype
+    if ft == "python" then
         split()
-		vim.cmd("term python3 %")
-	elseif ft == "markdown" then
-		vim.cmd(":MarkdownPreview")
-	elseif ft == 'c' then
-		split()
-		vim.cmd("term gcc % -o %< && ./%< && rm %<")
-	elseif ft == 'cpp' then
-		split()
-		vim.cmd("term g++ % -o %< && ./%< && rm %<")
-	elseif ft == 'javascript' then
-		split()
-		vim.cmd("term node %")
-	elseif ft == 'lua' then
-		split()
-		vim.cmd("term luajit %")
-	elseif ft == 'tex' then
-		vim.cmd(":VimtexCompile")
-	end
+        vim.cmd("term python3 %")
+    elseif ft == "markdown" then
+        vim.cmd(":MarkdownPreview")
+    elseif ft == "c" then
+        split()
+        vim.cmd("term gcc % -o %< && ./%< && rm %<")
+    elseif ft == "cpp" then
+        split()
+        vim.cmd("term g++ -pedantic-errors -std=c++11 % -o %< && ./%< && rm %<")
+    elseif ft == "javascript" then
+        split()
+        vim.cmd("term node %")
+    elseif ft == "lua" then
+        split()
+        vim.cmd("term luajit %")
+    elseif ft == "tex" then
+        vim.cmd(":VimtexCompile")
+    end
 end
 
 return {
     cmd = "RunCode",
-    func = compileRun
+    func = compileRun,
 }
