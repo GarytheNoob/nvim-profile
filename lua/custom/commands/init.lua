7b199986cc48c083e59d8ecee154b8ea86eaaf4c
ZhangXuecheng <zxc0gary@connect.hku.hk>
2026-05-01 16:25:01 +0800

    disable inslogo

--- lua/custom/commands/init.lua
+++ lua/custom/commands/init.lua
@@ -1,11 +1,10 @@
-local commands={
-    require("custom.commands.inslogo"),
+local commands = {
+    -- require("custom.commands.inslogo"),
     require("custom.commands.bufclose"),
     require("custom.commands.lazygit"),
     require("custom.commands.openterm"),
     require("custom.commands.runcode"),
 }
-
 
 for _, command in ipairs(commands) do
     vim.api.nvim_create_user_command(command.cmd, command.func, {})
