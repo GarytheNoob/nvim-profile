f0b0482d1db8e8e881f88487abe30bbbf3d30352
GarytheNoob on ArchLinux <garythenoob@outlook.com>
2026-06-25 15:48:15 +0800

    del hop jieba; merge mac

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,9 +1,11 @@
 return {
     {
-        "christoomey/vim-tmux-navigator"
+        "christoomey/vim-tmux-navigator",
     },
     {
-        'akinsho/toggleterm.nvim', version = "*", config = true
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        config = true,
     },
     {
         "norcalli/nvim-colorizer.lua",
@@ -14,22 +16,22 @@ return {
     {
         "RRethy/vim-illuminate",
         config = function()
-            require('illuminate').configure({
+            require("illuminate").configure({
                 providers = {
                     -- 'lsp',
                     -- 'treesitter',
-                    'regex',
+                    "regex",
                 },
             })
-        end
+        end,
     },
-    {
-        'noearc/jieba.nvim',
-        dependencies = {'noearc/jieba-lua'},
-        config = function ()
-            vim.keymap.set('n', 'ce', ":lua require'jieba_nvim'.change_w()<CR>", {noremap = false, silent = true})
-            vim.keymap.set('n', 'de', ":lua require'jieba_nvim'.delete_w()<CR>",  {noremap = false, silent = true})
-            vim.keymap.set('n', '<leader>w' , ":lua require'jieba_nvim'.select_w()<CR>", {noremap = false, silent = true})
-        end
-    },
+    -- {
+    --     'noearc/jieba.nvim',
+    --     dependencies = {'noearc/jieba-lua'},
+    --     config = function ()
+    --         vim.keymap.set('n', 'ce', ":lua require'jieba_nvim'.change_w()<CR>", {noremap = false, silent = true})
+    --         vim.keymap.set('n', 'de', ":lua require'jieba_nvim'.delete_w()<CR>",  {noremap = false, silent = true})
+    --         vim.keymap.set('n', '<leader>w' , ":lua require'jieba_nvim'.select_w()<CR>", {noremap = false, silent = true})
+    --     end
+    -- },
 }
